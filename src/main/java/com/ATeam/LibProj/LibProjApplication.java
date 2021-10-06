f1d5c46996e0775840efa0eba874eba3ef74601d
David <dpantech@gmail.com>
2021-10-06 12:49:23 -0400

    Initial Commit

---
+++ src/main/java/com/ATeam/LibProj/LibProjApplication.java
@@ -0,0 +1,13 @@
+package com.ATeam.LibProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ATeam/LibProj/LibProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ATeam.LibProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
